5b7b7dffa7fd38facb61cb1aa06f02b1b79bfa37
Quinch <quinch@iMac-de-admin.local>
2025-05-16 15:05:34 +0200

    tout fonctionne let's gooooo

---
+++ requirements/mariadb/setup_mdb.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DATABASE_DIR=/var/lib/mysql/${MYSQL_DATABASE}
+
+echo "🔧 Chargement des variables d’environnement..."
+echo "🔸 SQL_DATABASE       = ${MYSQL_DATABASE}"
+echo "🔸 SQL_USER           = ${MYSQL_USER}"
+echo "🔸 SQL_PASSWORD       = ${MYSQL_PASSWORD}"
+echo "🔸 SQL_ROOT_PASSWORD  = ${MYSQL_ROOT_PASSWORD}"
+if [ ! -d "$DATABASE_DIR" ]; then
+
+	# Launch mariadb in background
+	echo "dataBase not found... creation of ${MYSQL_DATABASE}"
+	/usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+
+	until mysqladmin ping ; do
+		echo "waiting for mysql_safe launche"
+		sleep 2
+	done
+
+	mysql -u root << EOF
+
+	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+	DELETE FROM mysql.user WHERE user='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+	DELETE FROM mysql.user WHERE user='';
+	CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+	FLUSH PRIVILEGES;
+
+EOF
+	# Stop mariadb => Restart it
+	killall mysqld 2> /dev/null
+	echo "data base created"
+
+fi
+exec "$@"
